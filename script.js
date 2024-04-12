93ea666673577c1824de0570ef5ab144949a42fc
rain-1107 <83094739+rain-1107@users.noreply.github.com>
2024-04-12 23:02:04 +0100

    Working win screen

--- script.js
+++ script.js
@@ -28,21 +28,17 @@ function isArrayInArray(parent, searchItem) {
   }
   return false
 }
-class Rectangle {
-  constructor(position, size) {
-    this.position = position;
-    this.size = size;
-  }
-  draw(colour) {
-    screen.fillStyle = colour;
-    screen.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
-  }
-  static drawRect(position, size, colour) {
-    screen.fillStyle = colour;
-    screen.fillRect(position.x, position.y, size.x, size.y);
-  }
+
+function drawText(text, position, size, colour) {
+  screen.font = "30px Poppins"
+  screen.fillStyle = colour;
+  screen.fillText(text, position.x, position.y);
 }
 
+function drawRect(position, size, colour) {
+  screen.fillStyle = colour;
+  screen.fillRect(position.x, position.y, size.x, size.y);
+}
 
 function drawGrid(position, size, colour, thickness = 20) {
   screen.lineWidth = thickness;
@@ -89,17 +85,20 @@ class Game {
   constructor() {
     this.backgroundColour = "#555555";
     this.nextTurn = "cross";
-    this.crossPositions = [];
+    this.crossPositions = [
+      [0, 0],
+      [1, 0],
+      [2, 0]
+    ];
     this.noughtPositions = [];
   }
   update(clickEvent = 0) {
-    console.log("Updating")
-      // Draw grid respective to canvas size
+    // Draw grid respective to canvas size
     let gridSize = 800 / 1080 * canvas.height;
     if (gridSize > canvas.width - 50) gridSize = canvas.width - 50;
     drawGrid({ x: canvas.width / 2 - gridSize / 2, y: canvas.height / 2 - gridSize / 2 }, gridSize, cGrid, 1 / 40 * gridSize)
 
-    if (clickEvent != 0) {
+    if (clickEvent != 0 && this.nextTurn != "") {
       // Convert click event to new cross/nought position
       let position = { x: clickEvent.clientX, y: clickEvent.clientY };
       if (canvas.width / 2 - gridSize / 2 < position.x && position.x < canvas.width / 2 - gridSize / 2 + gridSize &&
@@ -107,7 +106,6 @@ class Game {
         let x = Math.floor((position.x - (canvas.width / 2 - gridSize / 2)) / (gridSize / 3))
         let y = Math.floor((position.y - (canvas.height / 2 - gridSize / 2)) / (gridSize / 3))
         if (this.nextTurn == "cross") {
-          console.log("Adding cross", !(isArrayInArray(this.crossPositions, [x, y])), (this.noughtPositions.includes([x, y])))
           if (!(isArrayInArray(this.crossPositions, [x, y])) && !(isArrayInArray(this.noughtPositions, [x, y]))) {
             this.crossPositions.push([x, y]);
             if (this.crossPositions.length > 3) {
@@ -117,7 +115,6 @@ class Game {
           }
 
         } else {
-          console.log("Adding nought")
           if (!(isArrayInArray(this.crossPositions, [x, y])) && !(isArrayInArray(this.noughtPositions, [x, y]))) {
             this.noughtPositions.push([x, y]);
             if (this.noughtPositions.length > 3) {
@@ -127,6 +124,9 @@ class Game {
           }
         }
       }
+    }
+    if (this.nextTurn == "") {
+      this.nextTurn = "cross"
     }
     // Draw noughts and crosses
     for (let i = 0; i < this.crossPositions.length; i++) {
@@ -211,23 +211,22 @@ class Game {
     return 0;
   }
   showWinScreen(result) {
-    this.nextTurn = "cross";
+    this.nextTurn = "";
     this.noughtPositions = [];
     this.crossPositions = [];
     let text = "";
     if (result == "cross") {
-      text = "Crosses has won! Click anywhere to reset";
+      text = "Crosses has won!";
     } else {
-      text = "Noughts has won! Click anywhere to reset";
+      text = "Noughts has won!";
     }
     let rectWidth = 400;
     let rectHeight = 100;
     let thickness = 5
-    Rectangle.drawRect({ x: canvas.width / 2 - rectWidth / 2 - thickness, y: canvas.height / 2 - rectHeight / 2 - thickness }, { x: rectWidth + thickness * 2, y: rectHeight + thickness * 2 }, cGrid)
-    Rectangle.drawRect({ x: canvas.width / 2 - rectWidth / 2, y: canvas.height / 2 - rectHeight / 2 }, { x: rectWidth, y: rectHeight }, cBackground)
-    screen.font = "30px Poppins";
-    screen.fillStyle = cGrid;
-    screen.fillText(text, canvas.width / 2 - rectWidth / 2 - thickness.x, canvas.height / 2 - rectHeight / 2 - thickness);
+    drawRect({ x: canvas.width / 2 - rectWidth / 2 - thickness, y: canvas.height / 2 - rectHeight / 2 - thickness }, { x: rectWidth + thickness * 2, y: rectHeight + thickness * 2 }, cGrid)
+    drawRect({ x: canvas.width / 2 - rectWidth / 2, y: canvas.height / 2 - rectHeight / 2 }, { x: rectWidth, y: rectHeight }, cBackground)
+    drawText(text, { x: canvas.width / 2 - rectWidth / 4 - 32, y: canvas.height / 2 - 10 }, 30, cGrid)
+    drawText("Click anywhere to reset.", { x: canvas.width / 2 - rectWidth / 4 - 70, y: canvas.height / 2 + 23 }, 30, cGrid)
   }
 }
 
@@ -249,7 +248,7 @@ game = new Game();
 
 function update(event) {
   // Draw background
-  Rectangle.drawRect({ x: 0, y: 0 }, { x: canvas.width, y: canvas.height }, cBackground);
+  drawRect({ x: 0, y: 0 }, { x: canvas.width, y: canvas.height }, cBackground);
   game.update(event);
 }
 
